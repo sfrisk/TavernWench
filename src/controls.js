7ee78d3bc74950f3344067627cdd25eb8d9f33c2
Sarah Frisk <sarah@sarahfrisk.com>
2011-12-17 17:06:20 -0500

    players are aware of their own location

--- src/controls.js
+++ src/controls.js
@@ -2,19 +2,30 @@ var left = 37;
 var up = 38;
 var right = 39;
 var down = 40;
+var attack = 32;
 $(window).keypress(function(e){
 	var keyCode = e.keyCode;
+	var charcode = e.charCode;
 
+	if(charcode == attack){
+		e.preventDefault();
+		console.log("ATTACKING THE DARKNESS")	
+	}
+	
 	if(keyCode == down){
+		e.preventDefault();
 		game.board.moveDown();
 	}
 	if(keyCode == up){
+		e.preventDefault();
 		game.board.moveUp();
 	}
 	if(keyCode == right){
+		e.preventDefault();
 		game.board.moveRight();
 	}
 	if(keyCode == left){
+		e.preventDefault();
 		game.board.moveLeft();
 	}
 
